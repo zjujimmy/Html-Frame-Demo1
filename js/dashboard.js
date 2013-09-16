2dde955b2bbfc7dc6b9c8a939249f3fe2611b22c
zjujimmy <matrix5467@gmail.com>
2013-09-16 11:11:24 -0700

    table header not fixed

--- js/dashboard.js
+++ js/dashboard.js
@@ -98,18 +98,26 @@ $(document).ready(function () {
     		strNodeName += ".html?t=" + Math.random();
     		$('#grid-example').load(strNodeName, function(){
 
+
                     $.ajax({
                         type: "GET",
                         url: "../ListIncidentOverviewResp.json",
                         dataType:"json",
                         success: function(data){
                             $.each(data, function(i,n){
-                                var rec = "<tr>";
-                                rec += "<td id='entityId' >";
+                                var rec = "";
+                                /*if(i % 2 == 0){
+                                    rec += "<tr class='even'>";
+                                } else {
+                                    rec += "<tr class='odd'>";
+                                }*/
+                                rec +="<tr>";
+
+                                rec += "<td id='entityId'>";
                                 var eArray = n.entity.split(" ");
                                 $.each(eArray, function(i, value){
                                     if(i == 0){
-                                        rec += "<a href='content/sample-detail.html' target='_blank'>";
+                                        rec += "<a href='#'>";
                                         rec += eArray[i];
                                         rec += "</a><br/>";
                                     } else {
@@ -144,21 +152,104 @@ $(document).ready(function () {
                                 rec += "<td> </td>";
                                 rec += "</tr>";
                                 $('#mytable').append(rec);
-                            });
 
-                        }
+
+                                
+
+                            });
+                            
+
+
+                            $('#mytable a').click(function(){
+
+                                    $('#grid-example').load('content/sample-dd.html', function(){
+                                       $.ajax({
+                                                type: "GET",
+                                                url: "../IncidentDetail.json",
+                                                dataType:"json",
+                                                success: function(data){
+                                                    var entityId = data.entityId;
+                                                    $.each(data.incidentList, function(i,n){
+                                                        var irec = "<tr>";
+                                                        if(i % 2 == 0) {
+                                                            irec += "<td rowspan=2>" + entityId + "</td>";
+                                                        }
+                                                        
+                                                        irec += "<td>" + n.incidentClass + "</td>";
+                                                        var startTime = new Date(n.startTime * 1000);
+
+                                                        irec += "<td>" + startTime.toString() + "</td>";
+                                                        irec += "<td>" + n.endTime + "</td>";
+                                                        irec += "<td>";
+                                                        irec += "<button class='btn btn-success'><i class='icon-play icon-white'></i> Confirm</button> ";
+                                                        irec += "<button class='btn btn-danger'><i class='icon-play icon-white'></i> Dismiss</button> ";
+                                                        irec += "<button class='btn btn-warning'><i class='icon-play icon-white'></i> Ignore</button> ";
+                                                        irec += "<button class='btn'><i class='icon-play icon-white'></i> Whitelist</button> ";
+                                                        irec += "</td>";
+                                                        irec += "</tr>";
+                                                        $('#incidentTable').append(irec);
+                                                    });
+                                                    
+
+                                                    $.each(data.eventList, function(i,n){
+                                                        var seqEvent = i + 1;
+                                                        var rec = "<tr><td>" + seqEvent + "</td>";
+                                                        rec += "<td>" + n["type"] + "</td>";
+                                                        //rec += "<td>" + n["sourceList"] + "</td>";
+                                                        rec += "<td>";
+                                                        $.each(n.sourceList, function(i,item) {
+                                                            rec += item.ip + " :" + item.port + "<br/>";
+                                                        });
+                                                        rec += "</td>";
+                                                        rec += "<td>";
+                                                        $.each(n.targetList, function(i,item) {
+                                                            rec += item.ip + " :" + item.port + "<br/>";
+                                                        });
+                                                        rec += "</td>";
+
+                                                        //rec += "<td>" + n["targetList"] + "</td>";
+                                                        rec += "<td>" + n["type"] + "</td>";
+                                                        rec += "<td>" + n["flows"] + "</td>";
+                                                        rec += "<td>" + n["bytesUp"] + "</td>";
+                                                        rec += "<td>" + n["packets"] + "</td>";
+                                                        rec += "</tr>";
+                                                        $('#eventTable').append(rec);
+                                                        
+                                                    });
+                                                    
+
+                                                }//success
+                                        });//$.ajax
+                                        
+                                        
+                                    });//load detail page
+                                });//a click
+                            
+                            
+
+                        }//
+
+
+                        
                     });
 
- 
+ /*
                     $('#mytable').dataTable({
-                        "sScrollY": "400px",
+                        "sScrollY": "700px",
+                        "sScrollX": "100%",
                         "bPaginate": false,
                         "bScrollCollapse": true,
                         "bInfo": false,
-                        "bJQueryUI": true
+                        "bJQueryUI": true,
+                        "bSort": false,
+                        "bFilter": false,
+                        "sScrollXInner": "110%"
                     });
 
+*/
 
+
+                
 
             });
 
